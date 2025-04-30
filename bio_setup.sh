8901daa93be0cbabc34d7957bb4f22fa9ed8984e
Marek <mareksz19@gmail.com>
2025-04-30 15:02:41 +0200

    Create bio_setup.sh

---
+++ bio_setup.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+set -e  # Exit on error
+
+echo "Creating directories..."
+mkdir -p NGS
+cd NGS
+mkdir -p data/fastq reference adapters
+
+echo "Downloading test FASTQ files (subset)..."
+cd data
+fastq-dump --split-files -X 1000000 --gzip SRR32281629
+fastq-dump --split-files -X 1000000 --gzip SRR32281627
+cd ../
+
+echo "Downloading reference genome..."
+cd reference
+wget -c https://ftp.ensembl.org/pub/release-113/fasta/homo_sapiens/dna/Homo_sapiens.GRCh38.dna.primary_assembly.fa.gz
+cd ..
+
+echo "Downloading adapter sequences..."
+cd adapters
+wget -c https://raw.githubusercontent.com/usadellab/Trimmomatic/main/adapters/TruSeq3-PE.fa
+
+echo "Creating placeholder for polyA adapter..."
+echo ">polyA
+AAAAAAAAAAAAAAAAAAAA" > polyA.fa
+cd ..
+
+echo "Setup part 3 complete."
